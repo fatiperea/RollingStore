b3d46da2f05aa99ecb4af566c374c77e7a5d4fa8
fatiperea <fatimaperea3pppp0@gmail.com>
2023-12-14 12:24:09 -0300

    correcciones de sintaxis

--- js/app.js
+++ js/app.js
@@ -1,71 +1,76 @@
-
 import Producto from "./classProducto.js";
-
-
-
 
 //constantes
 
-const modalIngresoProducto = new bootstrap.Modal(document.getElementById("administrarProducto"));
-const btnIngresaProducto=document.getElementById("btnIngresaProducto");
-const formProducto=document.querySelector("form");
+const modalIngresoProducto = new bootstrap.Modal(
+  document.getElementById("administrarProducto")
+);
+const btnIngresaProducto = document.getElementById("btnIngresaProducto");
 
-const nombre=getElementById("nombre"),
-precio=getElementById("precio"),
-categoria=getElementById("categoria"),
-img=getElementById(img),
-descripcion=getElementById("descripcion"),
-stock=getElementById("stock");
+console.log("boton ingresa", btnIngresaProducto);
+
+const formProducto = document.querySelector("form");
+
+console.log("form ",formProducto);
+
+const nombre = document.getElementById("nombre"),
+  precio = document.getElementById("precio"),
+  categoria = document.getElementById("categoria"),
+  img = document.getElementById("imagen"),
+  descripcion = document.getElementById("descripcion"),
+  stock = document.getElementById("stock");
 
 const listaProductos = JSON.parse(localStorage.getItem("listaProduKey")) || [];
 
-  console.log("modal ",modalIngresoProducto);
+console.log("modal ", modalIngresoProducto);
 
 //funciones
 
-  const crearProducto = (e) => {
-    e.preventDefault();
-  
-      const producto = new Producto(
-        undefined,
-        nombre.value,
-        precio.value,
-        categoria.value,
-        img.value,
-        stock.value
-      );
-      console.log("prod ", producto);
-      
-      listaProductos.push(producto);
-  
-      console.log("lista prod ",listaProductos);
-  
-      formProducto.reset();
-  
-      //guardaLocalStorage();
-  
-      //llenarFila(producto, listaProducto.length);
-  
-      modalIngresoProducto.hide();
-  
-  };
-  
+const crearProducto = (e) => {
 
-  const mostrarModal = () => {
-    modalIngresoProducto.show();
-    formProducto.reset();
-  };
+  console.log("dentro de la fc");
 
+
+  e.preventDefault();
+  
+  const producto = new Producto(
+    undefined,
+    nombre.value,
+    precio.value,
+    categoria.value,
+    img.value,
+    descripcion.value,
+    stock.value
+  );
     
-  btnIngresaProducto.addEventListener("click", mostrarModal);
-  
 
-  formProducto.addEventListener("submit", crearProducto);
-  
-=======
-  //formContacto.addEventListener("submit", crearContacto);
-  
+  console.log("prod ", producto);
 
-import Producto from "./classProducto.js";
+  //listaProductos.push(producto);
+
+  //console.log("lista prod ", listaProductos);
+
+  //formProducto.reset();
+
+  //guardaLocalStorage();
+
+  //llenarFila(producto, listaProducto.length);
+
+  //modalIngresoProducto.hide();
+};
+
+function guardaLocalStorage() {
+  localStorage.setItem("listaProduKey", JSON.stringify(listaProductos));
+}
+
+
+const mostrarModal = () => {
+  modalIngresoProducto.show();
+  //formProducto.reset();
+};
+
+btnIngresaProducto.addEventListener("click", mostrarModal);
+
+formProducto.addEventListener("submit", crearProducto);
 
 
