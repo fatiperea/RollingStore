52e5f1001a7fa782ee0805f9486c01f0a29f2202
fatiperea <fatimaperea3pppp0@gmail.com>
2023-12-13 22:38:28 -0300

    inicio la logica en app.js sobre el btn para ingresar producto

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 
 
+/*const modalIngresoProducto = new bootstrap.Modal(
+    document.querySelector("modal")
+  );*/
+
+const modalIngresoProducto = new bootstrap.Modal(document.getElementById("administrarProducto"));
+const btnIngresaProducto=document.getElementById("btnIngresaProducto");
+  
+  console.log("modal ",modalIngresoProducto);
+
+  const mostrarModal = () => {
+    modalIngresoProducto.show();
+    formProducto.reset();
+  };
+    
+  btnIngresaProducto.addEventListener("click", mostrarModal);
+  
+  //formContacto.addEventListener("submit", crearContacto);
+  
