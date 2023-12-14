73d27c7e83ac7101ee058b0ee4c6bd2641010f8e
Nacho <lic.medinajimenez@gmail.com>
2023-12-14 13:01:05 -0300

    agrege js de login para admin

---
+++ js/login.js
@@ -0,0 +1,10 @@
+function login() {
+    let user.getElementById("usuario").value;
+    let pass.getElementById("pass").value;
+
+    if (user=="admin" && pass=="123") {
+        window.location="administracion.html"
+    } else {
+        alert("Usuario o contraseña incorrecta");
+    }
+}
