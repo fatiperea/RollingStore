20b5a353fc41e442fc00bebefe9b15229e9113e0
nachocode <lic.medinajimenez@gmail.com>
2023-12-15 21:30:52 -0300

    cree login.js y valide mail psw y user

--- js/login.js
+++ js/login.js
@@ -1,10 +1,29 @@
-function login() {
-    let user.getElementById("usuario").value;
-    let pass.getElementById("pass").value;
+function log() {
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+  const email = document.getElementById("email").value;
+//   const initBtn = document.getElementById("initBtn");
 
-    if (user=="admin" && pass=="123") {
-        window.location="administracion.html"
-    } else {
-        alert("Usuario o contraseña incorrecta");
-    }
+  if (user == "admin" && pass == "123" && email == "@") {
+    window.location = "administracion.html";
+  } else {
+    alert("Usuario o contraseña incorrecta");
+    document.getElementById("formLog").reset();
+
+  }
 }
+
+
+// initBtn.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     log()
+// }
+
+// initBtn.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     const data = {
+//         user: user.value,
+//         pass: pass.value
+//     }
+//     console.log(data)
+// })
