8098a00532d016761feff3807a29443d967499dd
Nacho <lic.medinajimenez@gmail.com>
2023-12-19 12:05:08 -0300

    merger

--- js/login.js
+++ js/login.js
@@ -1,14 +1,12 @@
 function log() {
-    const user = document.getElementById("user").value;
-    const pass = document.getElementById("pass").value;
-    const email = document.getElementById("email").value;
-  
-    if (user === "admin" && pass === "12345678" && email === "admin@admin.com") {
-      window.location = "administracion.html";
-    } else {
-      alert("Usuario o contraseña incorrecta");
-      document.getElementById("formLog").reset();
-  
-    }
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+  const email = document.getElementById("email").value;
+
+  if (user === "admin" && pass === "12345678" && email === "admin@admin.com") {
+    window.location = "administracion.html";
+  } else {
+    alert("Usuario o contraseña incorrecta");
+    document.getElementById("formLog").reset();
   }
-  
+}
